fafd8cec836b0039c448369fc6ae7bfd9671f8ce
Ricardo <ricardo.maraschini@gmail.com>
2016-09-20 18:50:12 -0300

    Looking for Environment variable
    Looking if environment variable `server' is defined. If no,
    bail out with an error message

--- js/middleware/index.js
+++ js/middleware/index.js
@@ -1,6 +1,13 @@
 "use strict";
 
+if (process.env.server == undefined) {
+	console.log("Environment variable `server' not set, bailing out.");
+	process.exit(1);
+}
+
+console.log("Going to connect on " + process.env.server);
+
 var Middleware = require('./classes/Middleware').Middleware;
 var app = new Middleware();
-app.setBrokerAddress("tcp://127.0.0.1:6969");
+app.setBrokerAddress(process.env.server);
 app.run();
